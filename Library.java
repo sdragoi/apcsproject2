846cea8bcf67d50cb4683ed3297d3180445662e4
dwhite2304 <80843142+dwhite2304@users.noreply.github.com>
2022-04-11 08:46:48 -0400

    Update Library.java

--- Library.java
+++ Library.java
@@ -7,5 +7,28 @@ public class Library {
 	public Library(ArrayList<Book> library) {
 		this.library = library; 
 	}
+	 public ArrayList<Book> sortByTitle(ArrayList<Book> books) {
+         for (int i = 0; i < books.size(); i++) {
+             for (int j = 0; j < books.size() - 1; j++) {
+                 if (books.get(j).title.charAt(0) > books.get(j + 1).title.charAt(0)) {
+                     Book temp = books.set(j, books.get(j + 1));
+                     books.set(j + 1, temp);
+                 } else if ((books.get(j).title.charAt(0) == books.get(j + 1).title.charAt(0))) {
+                     if (books.get(j).title.charAt(1) > books.get(j + 1).title.charAt(1)) {
+                         Book temp = books.set(j, books.get(j + 1));
+                         books.set(j + 1, temp);
+                     } else if (books.get(j).title.charAt(1) == books.get(j + 1).title.charAt(1)) {
+                         if (books.get(j).title.charAt(2) > books.get(j + 1).title.charAt(2)) {
+                             Book temp = books.set(j, books.get(j + 1));
+                             books.set(j + 1, temp);
+                         }
+                     }
+
+                 }
+             }
+
+         }
+         return books;
+     }
 
 }
